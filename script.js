342c6eed79bb7ab6dbb3f6a476adc03e252140d7
KumarGaurav09 <kumartuntun123789@gmail.com>
2026-06-02 23:24:20 +0530

    Added task add and delete functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Functionality will be added tomorrow
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const taskList = document.querySelector(".task-list");
+
+button.addEventListener("click", () => {
+    const taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        ${taskText}
+        <button class="delete-btn">Delete</button>
+    `;
+
+    taskList.appendChild(li);
+
+    input.value = "";
+
+    li.querySelector(".delete-btn").addEventListener("click", () => {
+        li.remove();
+    });
+});
